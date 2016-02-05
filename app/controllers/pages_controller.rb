83f65651534b7ebd21d976aac508f2a7a1db760e
Rodrigo33024 <Rodrigo33024@gmail.com>
2016-02-05 16:40:45 -0500

    Nest pages into subjects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,11 @@ class PagesController < ApplicationController
 
   before_action :confirm_logged_in
 
+  before_action :find_subject
+
   def index
-    @pages = Page.all
+    #@pages = Page.where(:subject_id => @subject_id).sorted
+    @pages = @subject.pages.sorted
   end
 
   def show
@@ -13,7 +16,7 @@ class PagesController < ApplicationController
   end
 
   def new
-    @page = Page.new
+    @page = Page.new({:subject_id => @subject.id, :name => "Default"})
     @subjects = Subject.order('position ASC')
     @page_count = Page.count + 1
   end
@@ -23,7 +26,7 @@ class PagesController < ApplicationController
 
     if @page.save
       flash[:notice] = 'Page created successfully.'
-      redirect_to(:action => 'index')
+      redirect_to(:action => 'index', :subject_id => @subject.id)
     else
       @subjects = Subject.order('position ASC')
       @page_count = Page.count + 1
@@ -43,7 +46,7 @@ class PagesController < ApplicationController
 
     if @page.update_attributes(pages_params)
       flash[:notice] = 'Page updated successfully.'
-      redirect_to(:action => 'show', :id => @page.id)
+      redirect_to(:action => 'show', :id => @page.id, :subject_id => @subject.id)
     else
       @subjects = Subject.order('position ASC')
       @page_count = Page.count
@@ -58,12 +61,18 @@ class PagesController < ApplicationController
   def destroy
     page = Page.find(params[:id]).destroy
     flash[:notice] = "Page '#{page.name}' destroyed successfully."
-    redirect_to(:action => 'index')
+    redirect_to(:action => 'index', :subject_id => @subject.id)
   end
 
   private
 
   def pages_params
     params.require(:page).permit(:subject_id, :name, :permalink, :position, :visible)
+  end
+
+  def find_subject
+    if params[:subject_id]
+      @subject = Subject.find(params[:subject_id])
+    end
   end
 end
